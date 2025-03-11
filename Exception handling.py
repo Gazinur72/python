533b810484cd9975fb30078cdf6924dbe8a3d94e
Gaz <karimovgaz72@gmail.com>
2025-03-11 22:20:08 +0500

    Написал 4 матеметические функции с вводом чисел и выводом информации

--- Exception handling.py
+++ Exception handling.py
@@ -1,7 +1,26 @@
+print("Сложение")
+num1 = float(input("Введите первое число: "))
+num2 = float(input("Введите второе число: "))
+result1 = num1 + num2
+print("Результат сложения:", result1)
+
+print("Вычитание")
+num1 = float(input("Введите первое число: "))
+num2 = float(input("Введите второе число: "))
+result1 = num1 - num2
+print("Результат:", result1)
+
+print("Умножение")
+num1 = float(input("Введите первое число: "))
+num2 = float(input("Введите второе число: "))
+result1 = num1 * num2
+print("Результат:", result1)
+
+print("Деление")
 try:
     num1 = float(input("Введите первое число: "))
     num2 = float(input("Введите второе число: "))
-    result = num1/ num2
+    result = num1 / num2
 
 except ValueError:
     print("Ошибка: введено не число!")
