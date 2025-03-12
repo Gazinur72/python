07b110c85021c4e7ce6fbec25a0c8fb4edaaf497
Gaz <karimovgaz72@gmail.com>
2025-03-12 18:35:32 +0500

    добавил обработчик ошибок

--- main.py
+++ main.py
@@ -1,32 +1,39 @@
-print("Калькулятор")
-num1 = float(input("Введите первое число:"))
-num2 = float(input("Введите второе число:"))
+try:
+    print("Калькулятор")
+    num1 = float(input("Введите первое число:"))
+    num2 = float(input("Введите второе число:"))
 
-message = '''
-Выберите математическую операцию:
+    message = '''
+    Выберите математическую операцию:
+    
+    + сложение
+    - вычитание
+    / деление
+    * умножение
+    '''
 
-+ сложение
-- вычитание
-/ деление
-* умножение
-'''
+    operation = input(message)
 
-operation = input(message)
+    if operation == '+':
+        print('Сложение')
+        result = num1 + num2
+    elif operation == '-':
+        print('Вычитание')
+        result = num1 - num2
+    elif operation == '/':
+        print('Деление')
+        result = num1 / num2
+    elif operation == '*':
+        print('Умножение')
+        result = num1 * num2
+    else:
+        print('Неизвестная операция')
 
-if operation == '+':
-    print('Сложение')
-    result = num1 + num2
-elif operation == '-':
-    print('Вычитание')
-    result = num1 - num2
-elif operation == '/':
-    print('Деление')
-    result = num1 / num2
-elif operation == '*':
-    print('Умножение')
-    result = num1 * num2
+except ValueError:
+    print("Ошибка: введено не число!")
+except ZeroDivisionError:
+    print("Ошибка: Деление на ноль!")
 else:
-    print('Неизвестная операция')
-
-print("Результат:", result)
-
+    print(f"Результат: {result}")
+finally:
+    print("Программа завершина.")
